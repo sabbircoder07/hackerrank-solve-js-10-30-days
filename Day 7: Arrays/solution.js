0b66b75bcd1ee62433fc83b0085ba44d795813ba
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2023-03-27 09:55:29 +0600

    Create solution.js

---
+++ Day 7: Arrays/solution.js
@@ -0,0 +1,70 @@
+/*
+Objective
+Today, we will learn about the Array data structure. Check out the Tutorial tab for learning materials and an instructional video.
+
+Task
+Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+
+Example
+
+
+Print 4 3 2 1. Each integer is separated by one space.
+
+Input Format
+
+The first line contains an integer,  (the size of our array).
+The second line contains  space-separated integers that describe array 's elements.
+
+Constraints
+
+Constraints
+
+, where  is the  integer in the array.
+Output Format
+
+Print the elements of array  in reverse order as a single line of space-separated numbers.
+
+Sample Input
+
+4
+1 4 3 2
+Sample Output
+
+2 3 4 1
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    
+    let reverseArray="";
+    for(let i = arr.length-1; i >= 0; i-- )
+    {
+        reverseArray += arr[i] + " "
+    }
+    console.log(reverseArray);
+}
+
